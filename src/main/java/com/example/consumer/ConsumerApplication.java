f38b61bfb011ba3c7ffe8a0a810217322230b8f7
Yury Yarovoy <yury.yarovoy@intexsoft.by>
2022-10-06 12:24:00 +0300

    Initial commit

---
+++ src/main/java/com/example/consumer/ConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.example.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+    /**
+     * Main method in application.
+     *
+     * @param args system arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
